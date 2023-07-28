2177a0527c8e65d1f4a696eb277ed0af1191e618
huseyin doğdu <66470348+huso987@users.noreply.github.com>
2023-07-28 12:36:25 +0300

    10.gün 2.commit
    add_two_ints_client

---
+++ 10.gün/my_py_pkg(6)/my_py_pkg/my_first_node.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+class Mynode(Node):
+
+    def __init__(self):
+        super().__init__("py_test")
+        self.counter_=0
+        self.get_logger().info("merhaba ros2!!!")
+        self.create_timer(0.5 , self.timer_callback)
+
+    def timer_callback(self):
+        self.counter_ +=1
+        self.get_logger().info("selam  "+str(self.counter_))
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Mynode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+  
+
+if __name__=="__main__":
+    main()
